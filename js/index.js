db56d662992f27e1c6f6cde61138c032cf7a5c85
Masataka Hisasue <masataka.hisasue@optim.co.jp>
2019-04-09 13:46:30 +0900

    feat: redirect auth

--- js/index.js
+++ js/index.js
@@ -5,9 +5,13 @@ window.onload = function () {
       is_join: {},
       is_survival: {},
       count: 0,
+      userid: "",
+      auth_code: "",
     },
     created: function(){
         setInterval(() => { this.count++ }, 1000)
+        this.userid = document.getElementById("userid").innerHTML;
+        this.auth_code = document.getElementById("auth_code").innerHTML;
         this.getSurvival();
         this.getJoin();
     },
@@ -17,17 +21,18 @@ window.onload = function () {
     },
     methods: {
         getSurvival : function() {
-            fetch("/survival/me", {
+            fetch("/survival/" + this.userid + "?code=" + this.auth_code, {
                 method: 'GET',
                 mode: 'cors',
             }).then((response) => {
                 return response.json();
             }).then((json) => {
+                console.log(json)
                 this.is_survival = json;
             });
         },
         getJoin : function() {
-            fetch("/join/me", {
+            fetch("/join/" + this.userid + "?code=" + this.auth_code, {
                 method: 'GET',
                 mode: 'cors',
             }).then((response) => {
